74d0d39187f1dd12eedb9582e2bb222f3cf671b1
Anirudh A V <anirudh.av02@gmail.com>
2023-04-08 19:55:55 +0530

    fix: Tab closing fixed
    Now tabs can be closed by clicking on the close button. Also, UI is improved.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { github as syntaxStyle } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import Dexie from 'dexie';
+import { GrFormClose } from 'react-icons/gr';
 
 function App() {
 	const [file, setFile] = useState<File | null>(null);
@@ -35,24 +36,26 @@ function App() {
 			console.log('File reader not supported');
 			return;
 		}
-		reader.onload = (event) => {
-			if (!event.target) {
-				console.log('No file selected');
-				return;
-			}
-			const fileContents = event.target.result;
-			console.log(fileContents);
-			setFileContents(fileContents as string);
-			const newTab = { id: openTabs.length + 1, name: file.name, contents: fileContents as string };
-			setOpenTabs([...openTabs, newTab]);
-			db.files.add({
-				name: file.name,
-				contents: fileContents as string,
-				extension: file.name.split('.').pop() as string,
-				lastModified: new Date(),
-			});
-		};
-		reader.readAsText(file);
+		if (file !== null) {
+			reader.onload = (event) => {
+				if (!event.target) {
+					console.log('No file selected');
+					return;
+				}
+				const fileContents = event.target.result;
+				console.log(fileContents);
+				setFileContents(fileContents as string);
+				const newTab = { id: openTabs.length + 1, name: file.name, contents: fileContents as string };
+				setOpenTabs([...openTabs, newTab]);
+				db.files.add({
+					name: file.name,
+					contents: fileContents as string,
+					extension: file.name.split('.').pop() as string,
+					lastModified: new Date(),
+				});
+			};
+			reader.readAsText(file);
+		}
 	};
 
 	const handleRecentFileClick = async (name: string) => {
@@ -68,16 +71,30 @@ function App() {
 
 	const handleCloseTabClick = (id: number) => {
 		const updatedTabs = openTabs.filter((tab) => tab.id !== id);
+		if (updatedTabs.length === 0) {
+			setFile(null);
+			setFileContents(null);
+		}
+
+		if (updatedTabs.length > 0) {
+			const lastTab = updatedTabs[updatedTabs.length - 1];
+			setFile({ name: lastTab.name, size: 0, type: "" } as File);
+			setFileContents(lastTab.contents);
+		}
 		setOpenTabs(updatedTabs);
 	};
 
 	return (
-		<div className="flex flex-col w-full">
-			<h1 className="text-2xl font-bold">File Upload</h1>
-			<div className="flex flex-col w-full">
-				<label className="text-xl font-bold">File</label>
-				<input type="file" onChange={handleFileUpload} />
-
+		<div className="flex flex-col w-full mt-3 justify-center items-center">
+			<h1 className="text-2xl font-bold my-4">File Upload</h1>
+			<div className="flex flex-col justify-center items-center w-full mx-5">
+				<div className={`w-4/5 border rounded-lg flex items-center`}>
+					<label className={`flex items-center justify-center text-sm font-medium  border  rounded-lg cursor-pointer focus:outline-none w-40 h-10`} htmlFor="file_input">Upload file</label>
+					<input className={`w-full text-sm hidden text-gray-800 border hover:bg-gray-600 border-gray-300 bg-gray-500 focus:outline-none rounded-lg cursor-pointer`} aria-describedby="file_input_help" id="file_input" type="file" onChange={handleFileUpload} />
+					<div className="w-full flex items-center justify-center">
+						<p className="text-sm text-gray-500">{file ? file.name : "No file selected"}</p>
+					</div>
+				</div>
 				{recentFiles.length > 0 && (
 					<>
 						<label className="text-xl font-bold mt-4">Recent Files</label>
@@ -91,16 +108,16 @@ function App() {
 					</>
 				)}
 				{openTabs.length > 0 && (
-					<div className="flex flex-col mt-4">
-						<label className="text-xl font-bold">Open Tabs</label>
+					<div className="flex flex-col self-start ml-5 mt-4">
+						<label className="text-xl font-bold my-5">Open Tabs</label>
 						<div className="flex flex-row">
 							{openTabs.map((tab) => (
-								<div key={tab.id} className="flex flex-col mx-2">
-									<button className="rounded-full bg-red-500 text-white font-bold w-4 h-4 flex items-center justify-center" onClick={() => handleCloseTabClick(tab.id)}>
-										x
-									</button>
-									<button className="mt-2 text-blue-600 hover:underline" onClick={() => setFileContents(tab.contents)}>
+								<div key={tab.id} className="flex items-center bg-slate-200 justify-center pr-2 mx-2 hover:bg-slate-300 rounded-md">
+									<button className=" text-slate-900 p-2" onClick={() => setFileContents(tab.contents)}>
 										{tab.name}
+									</button>
+									<button className="rounded-full hover:bg-red-200 text-white font-bold w-5 h-5 flex items-center justify-center" onClick={() => handleCloseTabClick(tab.id)}>
+										<GrFormClose className='w-3 h-3 text-slate-50 bg-transparent' />
 									</button>
 								</div>
 							))}
@@ -109,8 +126,8 @@ function App() {
 				)}
 
 				{fileContents && (
-					<div className="flex flex-col mt-4">
-						<SyntaxHighlighter language="javascript" style={syntaxStyle}>
+					<div className="flex flex-col mt-4 mx-auto">
+						<SyntaxHighlighter language="javascript" style={syntaxStyle} customStyle={{background: '#f1f1f1', width: '80%', borderRadius: '30px', scrollbarWidth: 'thin'}} showLineNumbers={true} wrapLines={true}>
 							{fileContents}
 						</SyntaxHighlighter>
 					</div>
